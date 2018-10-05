11746c6d47e32435fe218f671078a5079d30e9d2
Tilman Troester <tilman.troester@gmail.com>
2018-10-05 17:06:23 +0100

    Added Google drive utilities.

---
+++ baryon_painter/utils/google_drive.py
@@ -0,0 +1,47 @@
+import io
+import os
+
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+
+def get_folder_contents(service, folder_id):
+    """From https://stackoverflow.com/questions/36874223/why-wont-google-api-v3-return-children"""
+    query = f"'{folder_id}' in parents"
+
+    response = service.files().list(q=query,
+                                    spaces='drive',
+                                    fields='files(id, name, size)').execute()
+
+    return response["files"]
+
+def download_file(service, file_id, file_name, file_size, download_path):
+    print(f"Downloading {file_name}:", end="")
+
+    request = service.files().get_media(fileId=file_id)
+    downloaded = io.BytesIO()
+    downloader = MediaIoBaseDownload(downloaded, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        if status:
+            progress_percentage = status.progress() * 100
+            print(f" {progress_percentage:.1f}%", end="")
+    print("")
+
+    downloaded.seek(0)
+    
+    output_fn = os.path.join(download_path, file_name)
+    with open(output_fn, "wb") as f:
+        f.write(downloaded.read())
+
+def download_files_in_folder(folder_id, download_path):
+    drive_service = build('drive', 'v3')
+
+    file_infos = get_folder_contents(drive_service, folder_id)
+    
+    print(f"Writing downloaded files to {download_path}")
+    for file_info in file_infos:
+        download_file(drive_service, 
+                      file_info["id"], file_info["name"], file_info["size"], 
+                      download_path)
+    print("Done!")
