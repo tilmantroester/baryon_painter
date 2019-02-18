99006873c3311e2c629928ec664fc60d86e9a2ab
Tilman Troester <tilman.troester@gmail.com>
2019-02-18 13:48:54 +0000

    Updates to training script.

--- scripts/CVAE_single_scale.py
+++ scripts/CVAE_single_scale.py
@@ -36,6 +36,10 @@ if __name__ == "__main__":
                                                                               "pressure" : 4}, 
                                                                     modes={"dm":"shift-log",
                                                                            "pressure" : "shift-log"},
+#                                                                     k_values={"dm" : (0.01, 4.0), 
+#                                                                               "pressure" : (1.0, 0.3)}, 
+#                                                                     modes={"dm":"shift-log-2p",
+#                                                                            "pressure" : "shift-log-2p"},
 #                                                                     k_values={"dm" : (2,1), 
 #                                                                               "pressure" : (1,0)}, 
 #                                                                     modes={"dm":"x/(1+x)",
@@ -95,6 +99,9 @@ if __name__ == "__main__":
                         "dim_z" :       dim_z,
                         "n_x_features": n_x_feature,
                         "aux_label" :   True,
+#                         "prior_z_y" :     cvae.conv_down(in_channel=1+n_aux_label, channels=[8,16,32], scales=[2,4,4])
+#                                         + cvae.conv_block(32, 2*dim_z[0], kernel=5)
+#                                         + [("unflatten", (2, *dim_z)),],
                         "q_x_in" :      cvae.conv_down(in_channel=n_x_feature, channels=[8,16,32], scales=[2,4,4]),
                         "q_y_in" :      cvae.conv_down(in_channel=1+n_aux_label, channels=[8,16,32], scales=[2,4,4]),
                         "q_x_y_out" :     cvae.conv_block(64, 2*dim_z[0], kernel=5)
@@ -107,19 +114,19 @@ if __name__ == "__main__":
                                            ("residual block", cvae.res_block(128)),
                                            ("residual block", cvae.res_block(128)),
                                            ("residual block", cvae.res_block(128)),
-                                           ("residual block", cvae.res_block(128)),
-                                           ("residual block", cvae.res_block(128)),
-                                           ("residual block", cvae.res_block(128)),
-                                           ("residual block", cvae.res_block(128)),
+#                                            ("residual block", cvae.res_block(128)),
+#                                            ("residual block", cvae.res_block(128)),
+#                                            ("residual block", cvae.res_block(128)),
+#                                            ("residual block", cvae.res_block(128)),
                                           ]
                                         + cvae.conv_up(128, channels=[64,32,16], scales=[2,2,2], bias=False, batchnorm=True, activation="ReLU"),
                         "p_y_z_out" :   (# Mu 
-                                          cvae.conv_block(16, 8, kernel=7, bias=False, batchnorm=False, activation="ReLU")
+                                          cvae.conv_block(16, 8, kernel=7, bias=False, batchnorm=False, activation="PReLU")
                                         + cvae.conv_block(8, n_x_feature, kernel=5, bias=False, batchnorm=False, activation="PReLU")
                                         + cvae.conv_block(n_x_feature, n_x_feature, kernel=3, bias=False, batchnorm=False, activation="softplus"),
     #                                      # Var
-#                                           cvae.conv_block(16, 8, kernel=7, bias=False, batchnorm=False, activation="PReLU")
-#                                         + cvae.conv_block(8, n_x_feature, kernel=5, bias=False, batchnorm=False, activation="PReLU")
+#                                           cvae.conv_block(16, 8, kernel=7, bias=False, batchnorm=False, activation="ReLU")
+#                                         + cvae.conv_block(8, n_x_feature, kernel=5, bias=False, batchnorm=False, activation="ReLU")
 #                                         + cvae.conv_block(n_x_feature, n_x_feature, kernel=3, bias=False, batchnorm=False, activation=None)
                                         ),
                         "min_x_var" :   1e-7,
@@ -135,18 +142,21 @@ if __name__ == "__main__":
                                                  test_data_set=validation_dataset,
                                                  architecture=test_net, 
                                                  compute_device=compute_device)
-
+        
+    print(painter.model)
+    
     def adaptive_batch_size(pepoch, min_batch_size=1, max_batch_size=24):
 #         return min(max_batch_size, min_batch_size*2**(pepoch//8))
-        steps = [(2, 2), (6, 4), (14, 8), (24, 16), (40, 24)]
+#         steps = [(2, 2), (6, 4), (14, 8), (24, 16), (40, 24)]
+        steps = [(0, 4), (8, 8), (16, 16), (32, 24)]
         for s in steps[::-1]:
             if pepoch >= s[0]:
                 return min(s[1], max_batch_size)
         return min_batch_size
 
     def adaptive_lr(pepoch):
-        step = 24
-        min_pepoch = 80-step
+        step = 64
+        min_pepoch = 96-step
         min_lr = 1e-8
 
         if pepoch < min_pepoch:
@@ -155,19 +165,29 @@ if __name__ == "__main__":
             gamma = 0.5
             return max(min_lr, 0.5**((pepoch-min_pepoch)//step))
 
-    run_name = "single_scale_max_z2_no_log_shift_softmax_lr1e-3"
-    painter.train(n_epoch=2, n_pepoch=256, learning_rate=1e-3, batch_size=4,
+    
+    run_name = "single_scale_max_z2_res4_late_prelu_log_shift_softmax_lr1e-3_slow_decay"
+    output_path = os.path.join(output_path, run_name)
+    os.makedirs(output_path)
+    with open(os.path.join(output_path, "architecture.txt"), "w") as f:
+        f.write(repr(painter.model.architecture))
+    with open(os.path.join(output_path, "architecture_built.txt"), "w") as f:
+        f.write(repr(painter.model))
+        
+    painter.train(n_epoch=1, n_pepoch=109, learning_rate=1e-3, batch_size=4,
                   adaptive_learning_rate=adaptive_lr, 
                   adaptive_batch_size=adaptive_batch_size,
                   pepoch_size=1568,
-                  validation_pepochs=[0,1,2,5,10,20,30,40,50,60,70,80,90,100,110,120], validation_batch_size=8,
-                  checkpoint_frequency=10000, statistics_report_frequency=400, 
-                  loss_plot_frequency=4000, mavg_window_size=50,
+                  validation_loss_frequency=72, 
+                  validation_loss_batch_size=24,
+                  validation_pepochs=[0,1,2,5,10,15,20,30,40,50,60,70,80,90,100,120,140,160,180,200,230,260,290,350,400], validation_batch_size=8,
+                  checkpoint_frequency=20000, statistics_report_frequency=400, 
+                  loss_plot_frequency=0, mavg_window_size=50,
                   show_plots=False,
                   save_plots=True,
                   plot_sample_var=False,
                   plot_power_spectra=["auto", "cross"],
                   plot_histogram=["log"],
-                  output_path=os.path.join(output_path, run_name),
+                  output_path=output_path,
                   verbose=True)
 
